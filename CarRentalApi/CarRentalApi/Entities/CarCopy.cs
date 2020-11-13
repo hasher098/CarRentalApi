b7ea543fa55b633576a1fd4ccf3beb590602cfa5
DESKTOP-3UGII65\Kamil <michal55568@gmail.com>
2020-11-13 01:42:36 +0100

    seedy wszystkich tabel

--- CarRentalApi/CarRentalApi/Entities/CarCopy.cs
+++ CarRentalApi/CarRentalApi/Entities/CarCopy.cs
@@ -9,7 +9,7 @@ using System.ComponentModel.DataAnnotations.Schema;
         
         public int Id { get; set; }
         [Required]
-        [MaxLength(8),MinLength(7)]
+        [MaxLength(10)]
         public string RegistrationNumber { get; set; }
         //numer rejestracyjny nie tylko zawiera liczby, dlatego int raczej siê tu nie przyda
         [ForeignKey("Car")]
